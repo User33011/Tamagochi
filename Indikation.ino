8afc4553c1d5c2ce00bf19db447ab86aab837276
Shifoner <79485569+User33011@users.noreply.github.com>
2024-12-05 19:26:40 +0800

    Update Indikation.ino
    Функция исправлена, работает навигация, не работало из-за таймера?

--- Indikation.ino
+++ Indikation.ino
@@ -1,58 +1,47 @@
 void drawFrame(int x, int y) {
     oled.rect(x - 2, y - 2, x +26, y+26, OLED_STROKE);
 }
-void indikat() {
-    const int itemCount = 8;
-    while (true) {
-        oled.clear();
-        button.flag = false;
-        button.A = digitalRead(BUTTON_A);
-        button.B = digitalRead(BUTTON_B);
-        button.C = digitalRead(BUTTON_C);
-        button.D = digitalRead(BUTTON_D);
-        
-        // Навигация по иконкам
-        // Переход вверх по строкам.
-        if (!button.A && !button.flag && millis() - button.timer > 100) {
-            button.flag = true;
-            button.timer = millis();
-            break;
-        } else if (!button.B && !button.flag && millis() - button.timer > 100) {
-            button.flag = true;
-            button.timer = millis();
-            SelectedOption(currentCondition);
-            break;
-        } else if (!button.C && !button.flag && millis() - button.timer > 100) {
-            button.flag = true;
-            button.timer = millis();
-            currentCondition = (currentCondition + 1) % NUM_PET_CONDITIONS;
-        } else if (!button.D && !button.flag && millis() - button.timer > 100) {
-            button.flag = true;
-            button.timer = millis();
-            currentCondition = (currentCondition - 1) % NUM_PET_CONDITIONS;
-            if  (currentCondition < 0) currentCondition = MAX_ICONS;
-        } else {
-            button.flag = false; // Сброс флага при отсутствии нажатий.
-            button.timer = millis();
-        }
-        
-        // Отрисовка иконок============================================
-        for (int i = 0; i < itemCount; i++) {
-            int index = i; // В данном случае просто копируем индекс, так как мы отображаем все иконки.
-            int x = 5 + (i % 4) * (24 + 5); // 4 иконки по X
-            int y = 5 + (i / 4) * (24 + 5); // 2 ряда по Y
 
-            // Отображение иконки на экране.
-            oled.drawBitmap(x, y, petConditions[index].icon, 24, 24, BITMAP_NORMAL, BUF_ADD);
-            if (index == currentCondition) {
-                drawFrame(x, y); // Рамка вокруг активного состояния.
-            }
-        }
-        //=============================================================
-        // Если это текущая иконка, отобразить рамку.
-        oled.update();
+
+void indikat() {
+  const int itemCount = 8; 
+  uint32_t BUTON_TIMER = millis(); 
+  while (true) {
+    const int btnA = digitalRead(BUTTON_A);
+    const int btnB = digitalRead(BUTTON_B);
+    const int btnC = digitalRead(BUTTON_C);
+    const int btnD = digitalRead(BUTTON_D);
+        
+    if (!btnA && millis() - BUTON_TIMER > 100) {
+      BUTON_TIMER = millis();
+      break;
+    } else if (!btnB && millis() - BUTON_TIMER > 100) {
+      BUTON_TIMER = millis();
+      SelectedOption(currentCondition);
+    } else if (!btnC && millis() - BUTON_TIMER > 100) {
+      BUTON_TIMER = millis();
+      currentCondition = (currentCondition - 1) % NUM_PET_CONDITIONS;
+        if  (currentCondition < 0) currentCondition = MAX_ICONS;
+    } else if (!btnD && millis() - BUTON_TIMER > 100) {
+      BUTON_TIMER = millis();
+      currentCondition = (currentCondition + 1) % NUM_PET_CONDITIONS;
+    } else {
+      BUTON_TIMER = millis();
     }
+    oled.clear();
+    for (int i = 0; i < itemCount; i++) {
+      int index = i;
+      int x = 5 + (i % 4) * (24 + 5);
+      int y = 5 + (i / 4) * (24 + 5);
+      oled.drawBitmap(x, y, petConditions[index].icon, 24, 24, BITMAP_NORMAL, BUF_ADD);
+        if (index == currentCondition) {
+          drawFrame(x, y); // Рамка вокруг активного состояния.
+        }
+    }
+    oled.update();
+  }
 }
+
 void SelectedOption(int index) {
     switch (index) {
         case 0:
@@ -64,7 +53,7 @@ void SelectedOption(int index) {
             // Здесь может быть код для выполнения действия пункта 2
             break;
         case 2:
-           playGame();
+            game_loop();
             // Здесь может быть код для выполнения действия пункта 2
             break;
         case 3:
