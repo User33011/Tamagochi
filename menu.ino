17afa1294960d39970accc31a5ca8b6fca17a566
Shifoner <79485569+User33011@users.noreply.github.com>
2024-12-05 19:27:33 +0800

    Update menu.ino
    Включена функция status_loop()

--- menu.ino
+++ menu.ino
@@ -1,60 +1,3 @@
-void bot_loop() {
-    uint16_t btnB = 0, btnC = 0, btnD = 0;
-    const uint16_t threshold = 5000; // Пороговое значение нажатий
-    int lastSelectedIndex = selectedIndex; // Для сравнения состояний
-
-    while (true) {
-        // Чтение состояния кнопок
-        bool buttonBPressed = !digitalRead(BUTTON_B);
-        bool buttonCPressed = !digitalRead(BUTTON_C);
-        bool buttonDPressed = !digitalRead(BUTTON_D);
-
-        // Обработка нажатия кнопок
-        if (buttonBPressed) {
-            btnB++;
-        } else {
-            btnB = 0;
-        }
-        if (buttonCPressed) {
-            btnC++;
-        } else {
-            btnC = 0;
-        }
-        if (buttonDPressed) {
-            btnD++;
-        } else {
-            btnD = 0;
-        }
-        // Проверка нажатий и выполнение действий
-        if (btnB > threshold) {
-            executeSelectedOption(selectedIndex);
-            btnB = 0; // Сбрасываем счетчик после выполнения
-        } 
-        if (btnC > threshold) {
-            selectedIndex--;
-            if (selectedIndex < 0) {
-                selectedIndex = menuCount - 1; // Зацикливаем вниз
-            }
-            if (lastSelectedIndex != selectedIndex) {
-                showMenu(selectedIndex); // Обновляем меню
-                lastSelectedIndex = selectedIndex; // Обновляем индекс
-            }
-            btnC = 0; // Сбрасываем счетчик
-        } 
-        if (btnD > threshold) {
-            selectedIndex++;
-            if (selectedIndex >= menuCount) {
-                selectedIndex = 0; // Зацикливаем вверх
-            }
-            if (lastSelectedIndex != selectedIndex) {
-                showMenu(selectedIndex); // Обновляем меню
-                lastSelectedIndex = selectedIndex; // Обновляем индекс
-            }
-            btnD = 0; // Сбрасываем счетчик
-        }
-    }
-}
-
 void showMenu(int index) {
     oled.clear(); // Очистка дисплея
     // Отображаем пункты меню
@@ -86,7 +29,7 @@ void executeSelectedOption(int index) {
             //debug();
             break;
         case 2:
-            //status_loop();
+            status_loop();
             break;
         case 3:
             displayTime();
@@ -95,7 +38,7 @@ void executeSelectedOption(int index) {
             tama_draw();
             break;
         case 5:
-            game_loop();
+            //game_loop();
             break;
         default:
             break;
