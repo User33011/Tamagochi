6d2765c62e3efcc8ce698c5ae902b44c2f83504d
Shifoner <79485569+User33011@users.noreply.github.com>
2025-01-17 23:01:04 +0800

    Update time.ino
    Добавлено отображения "0" если , секунды, минуты, часы, дата и месяц < 10

--- time.ino
+++ time.ino
@@ -5,16 +5,31 @@ void displayTime() {
   oled.setScale(1);
   oled.home();
   oled.setCursor(0, 0);
+  if (rtc.getHours() < 10) {
+        oled.print("0");
+    }
   oled.print(rtc.getHours());
   oled.print(":");
+  if (rtc.getMinutes() < 10) {
+        oled.print("0");
+    }
   oled.print(rtc.getMinutes());
   oled.print(":");
+  if (rtc.getSeconds() < 10) {
+        oled.print("0");
+    }
   oled.print(rtc.getSeconds());
   oled.print(" ");
   oled.print(rtc.getDay());
   oled.print(" ");
+  if (rtc.getDate() < 10) {
+        oled.print("0");
+    }
   oled.print(rtc.getDate());
   oled.print("/");
+  if (rtc.getMonth() < 10) {
+        oled.print("0");
+    }
   oled.print(rtc.getMonth());
   oled.print("/");
   oled.println(rtc.getYear());
